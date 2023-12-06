f45335e90637eacc8f85cd4a55ba193a0fed9b17
leon <l.elliottfuller@gmail.com>
2023-12-06 10:32:27 +0100

    Part 2 Day6 done, and cleaned code

--- AOC-2023/Day6/day6.py
+++ AOC-2023/Day6/day6.py
@@ -7,16 +7,24 @@ lines = data.split("\n")
 times = [int(num) for num in re.findall(r"(\d+)", lines[0])]
 distances = [int(num) for num in re.findall(r"(\d+)", lines[1])]
 
-out = 1
+time_part2 = int(''.join(re.findall(r"(\d+)", lines[0])))
+distance_part2 = int(''.join(re.findall(r"(\d+)", lines[1])))
 
-for time, dist in zip(times, distances):
+def beat_record(time: int, distance: int) -> int:
     count = 0
     for speed in range(1, time):
         total = (time - speed) * speed
 
-        if total > dist:
+        if total > distance:
             count += 1
 
-    out *= count
+    return count
 
-print(out)
+part1 = 1
+
+for time, dist in zip(times, distances):
+    count = beat_record(time, dist)
+    part1 *= count
+
+print('Part 1:', part1)
+print('Part 2:', beat_record(time_part2, distance_part2))
