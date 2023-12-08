811bf6cc8355ceffb38a272d3d23dfb03d4a7b13
leon <l.elliottfuller@gmail.com>
2023-12-08 18:04:10 +0100

    Part 2 Day8 completed but need to optimize

--- AOC-2023/Day8/day8.py
+++ AOC-2023/Day8/day8.py
@@ -3,12 +3,16 @@ import re
 data = open("input.txt").read().strip().split("\n")
 
 instructions = data[0]
-elements = data[2:-1]
+elements = data[2:len(data)]
 nodes = {}
+starting_nodes = []
 
 for node in elements:
     source, left, right = re.findall(r"([A-Z]{3})", node)
     nodes[source] = [left, right]
+    if source[2] == "A":
+        starting_nodes.append(source)
+
     
 start_node = "AAA"
 index = 0
@@ -20,3 +24,20 @@ while start_node != "ZZZ":
     steps += 1
 
 print("Part 1:", steps)
+
+# Part 2
+index = 0
+steps = 0
+//TODO: optimize this
+while not all(node[2] == "Z" for node in starting_nodes):
+    current_nodes_index = 0
+    current_nodes = []
+    for node in starting_nodes:
+        left, right = nodes[node]
+        current_nodes.append(left if instructions[index] == "L" else right)
+    index = (index + 1) % len(instructions)
+    starting_nodes = current_nodes
+    print(steps > 20685524831999)
+    steps += 1
+
+print("Part 2:", steps)
