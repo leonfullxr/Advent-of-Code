3e0aae069e780c44c94fe521f4b36a126f5d1f0e
leon <l.elliottfuller@gmail.com>
2023-12-09 11:03:37 +0100

    Day9 completed and cleaned up code

--- AOC-2023/Day9/day9.py
+++ AOC-2023/Day9/day9.py
@@ -1,60 +1,43 @@
-data = open("input.txt").read().strip().split("\n")
-
-# Part 1
-total_sum = 0
-
-for line in data:
-    initial_numbers = line.split(" ")
-    
-    for i in range(len(initial_numbers)):
-        initial_numbers[i] = int(initial_numbers[i])
-        
+def process_numbers(initial_numbers, is_part_1):
     additional_numbers = [initial_numbers]
 
     while additional_numbers[-1].count(0) != len(additional_numbers[-1]):
-        new_numbers = []
-        
-        for index in range(len(additional_numbers[-1])-1):
-            new_numbers.append(int(additional_numbers[-1][index+1]) - int(additional_numbers[-1][index]))
+        new_numbers = [int(additional_numbers[-1][i + 1]) - int(additional_numbers[-1][i]) for i in range(len(additional_numbers[-1]) - 1)]
         additional_numbers.append(new_numbers)
-    
-    additional_numbers[-1].append(0)
-    
-    for index in range(len(additional_numbers)-2, -1, -1):
-        additional_numbers[index].append(int(additional_numbers[index][-1]) + int(additional_numbers[index+1][-1]))
-        
-    total_sum += int(additional_numbers[0][-1])
-    
-print('Part 1:', total_sum)
-        
-# Part 2
-# Now, instead of adding an element to the end of the list, insert it at the start
-# This means that the last element of the list will be the first element of the list
-# And the first element of the list will be the last element of the list
-# This is a lot easier to work with
-total_sum = 0
 
-for line in data:
-    initial_numbers = line.split(" ")
-    
-    for i in range(len(initial_numbers)):
-        initial_numbers[i] = int(initial_numbers[i])
-        
-    additional_numbers = [initial_numbers]
+    if is_part_1:
+        additional_numbers[-1].append(0)
+        for index in range(len(additional_numbers) - 2, -1, -1):
+            additional_numbers[index].append(int(additional_numbers[index][-1]) + int(additional_numbers[index + 1][-1]))
+        return int(additional_numbers[0][-1])
+    else:
+        additional_numbers[-1].insert(0, 0)
+        for index in range(len(additional_numbers) - 2, -1, -1):
+            additional_numbers[index].insert(0, int(additional_numbers[index][0]) - int(additional_numbers[index + 1][0]))
+        return int(additional_numbers[0][0])
 
-    while additional_numbers[-1].count(0) != len(additional_numbers[-1]):
-        new_numbers = []
-        
-        for index in range(len(additional_numbers[-1])-1):
-            new_numbers.append(int(additional_numbers[-1][index+1]) - int(additional_numbers[-1][index]))
-        additional_numbers.append(new_numbers)
-    
-    additional_numbers[-1].insert(0, 0)
-    
-    for index in range(len(additional_numbers)-2, -1, -1):
-        additional_numbers[index].insert(0, int(additional_numbers[index][0]) - int(additional_numbers[index+1][0]))
-        
-    total_sum += int(additional_numbers[0][0])
 
-print('Part 2:', total_sum)
-    
+def main():
+    data = open("input.txt").read().strip().split("\n")
+
+    # Part 1
+    total_sum_part_1 = 0
+
+    for line in data:
+        initial_numbers = [int(num) for num in line.split(" ")]
+        total_sum_part_1 += process_numbers(initial_numbers, True)
+
+    print('Part 1:', total_sum_part_1)
+
+    # Part 2
+    total_sum_part_2 = 0
+
+    for line in data:
+        initial_numbers = [int(num) for num in line.split(" ")]
+        total_sum_part_2 += process_numbers(initial_numbers, False)
+
+    print('Part 2:', total_sum_part_2)
+
+
+if __name__ == "__main__":
+    main()
