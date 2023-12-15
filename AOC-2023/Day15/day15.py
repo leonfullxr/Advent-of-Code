3e1a39a88115b5e6cd3106239406f30d8bf7bdeb
leon <l.elliottfuller@gmail.com>
2023-12-15 19:27:30 +0100

    Da15 cleaned code

--- AOC-2023/Day15/day15.py
+++ AOC-2023/Day15/day15.py
@@ -1,80 +1,54 @@
-from typing import List
+from typing import List, Tuple
 
-def hash(s: str) -> int:
+def calculate_hash(s: str) -> int:
     v = 0
-    
     for ch in s:
-        v += ord(ch)
-        v *= 17
-        v %= 256
-
+        v = (v + ord(ch)) * 17 % 256
     return v
 
-def parse_part(part: str) -> tuple:
+def parse_part(part: str) -> Tuple[str, str, str]:
     label, operation, number = "", "", ""
-    
     for ch in part:
-        if ch == "-" or ch == "=":
+        if ch in "-=":
             operation = ch
         elif ch.isdigit():
             number += ch
         else:
             label += ch
-    
     return label, operation, number
 
-def part2(part: str, boxes: List[List[tuple]]) -> None:
+def update_boxes(part: str, boxes: List[List[Tuple[str, str]]]) -> None:
     label, operation, number = parse_part(part)
-    box_index = hash(label)
-    
+    box_index = calculate_hash(label)
     box = boxes[box_index]
-    num_lens = 0
 
-    for _label, _ in box:
-        if label == _label:
-            num_lens += 1
-    
     if operation == "=":
-        if num_lens > 0:
-            for i in range(len(box)):
-                if label == box[i][0]:
-                    box[i] = (label, number)
+        for i, (box_label, _) in enumerate(box):
+            if label == box_label:
+                box[i] = (label, number)
+                break
         else:
             box.append((label, number))
-    
-    elif operation == "-":
-        if len(box) == 0:
-            return
-        else:
-            index = -1
-            for i in range(len(box)):
-                if label == box[i][0]:
-                    index = i
-                    break
-            if index != -1:
-                box.pop(index)
-                    
-    
+
+    elif operation == "-" and box:
+        box[:] = [(box_label, lens) for box_label, lens in box if box_label != label]
+
+def calculate_total(boxes: List[List[Tuple[str, str]]]) -> int:
+    total = 0
+    for idx, box in enumerate(boxes):
+        power = sum((1 + idx) * (box_idx + 1) * int(lens) for box_idx, (_, lens) in enumerate(box))
+        total += power
+    return total
+
 def main() -> None:
     data = open("input.txt").read().strip().split(",")
-    total = 0
     boxes = [[] for _ in range(256)]
     
     for part in data:
-        total += hash(part)
-        part2(part, boxes)
+        update_boxes(part, boxes)
     
-    print('Part 1:', total)
-    
-    total = 0
-    for idx, box in enumerate(boxes):
-        power = 0
-        for box_idx, (_, lens) in enumerate(box):
-            power += (1 + idx) * (box_idx + 1) * int(lens)
-
-        total += power
-
-    print('Part 2:', total)
+    print('Part 1:', sum(calculate_hash(part) for part in data))
+    print('Part 2:', calculate_total(boxes))
 
 if __name__ == "__main__":
     main()
