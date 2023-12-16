afa74a819c80057c21e5c0260b81a0cfb7a5a092
leon <l.elliottfuller@gmail.com>
2023-12-16 01:48:13 +0100

    Day14 part1 cleaned code

--- AOC-2023/Day14/day14.py
+++ AOC-2023/Day14/day14.py
@@ -1,22 +1,30 @@
-data = [list(line) for line in open("input.txt").read().strip().split("\n")]
-length = len(data)
-width = len(data[0])
+def tilt_forward(matrix):
+    for row_idx, line in enumerate(matrix):
+        for col_idx, value in enumerate(line):
+            if value == "O":
+                decreasing_row_idx = row_idx - 1
+                while matrix[decreasing_row_idx][col_idx] == "." and decreasing_row_idx >= 0:
+                    matrix[decreasing_row_idx][col_idx] = "O"
+                    matrix[decreasing_row_idx+1][col_idx] = "."
+                    decreasing_row_idx -= 1
+                    
+def get_score(matrix):
+    length = len(matrix)
+    mult = length
+    sum = 0
+    for line in matrix:
+        for value in line:
+            if value == "O":
+                sum += mult
+        mult -= 1
+    return sum
 
-for row_idx, line in enumerate(data):
-    for col_idx, value in enumerate(line):
-        if value == "O":
-            decreasing_row_idx = row_idx - 1
-            while data[decreasing_row_idx][col_idx] == "." and decreasing_row_idx >= 0:
-                data[decreasing_row_idx][col_idx] = "O"
-                data[decreasing_row_idx+1][col_idx] = "."
-                decreasing_row_idx -= 1
-mult = length
-sum = 0
-for row_idx, line in enumerate(data):
-    print(line)
-    for col_idx, value in enumerate(line):
-       if value == "O":
-           sum += mult
-    mult -= 1
-           
-print('Part 1', sum) 
+def main():
+    data = [list(line) for line in open("input.txt").read().strip().split("\n")]
+    
+    tilt_forward(data)
+            
+    print('Part 1', get_score(data)) 
+
+if __name__ == "__main__":
+    main()
