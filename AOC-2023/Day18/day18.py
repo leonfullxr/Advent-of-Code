87fd9b380e415427e59f9502792c2a2bec20de0b
leon <l.elliottfuller@gmail.com>
2023-12-18 12:59:40 +0100

    Day18 part1 optimized

--- AOC-2023/Day18/day18.py
+++ AOC-2023/Day18/day18.py
@@ -1,97 +1,45 @@
 from collections import deque
-   
-def point_inside(x, y, path_points) -> bool:    
-    num = len(path_points)
-    j = num - 1
-    c = False
-    for i in range(num):
-        if (x == path_points[i][0]) and (y == path_points[i][1]):
-            # point is a corner
-            return True
-        if (path_points[i][1] > y) != (path_points[j][1] > y):
-            slope = (x - path_points[i][0]) * (path_points[j][1] - path_points[i][1]) - (
-                path_points[j][0] - path_points[i][0]
-            ) * (y - path_points[i][1])
-            if slope == 0:
-                # point is on boundary
-                return True
-            if (slope < 0) != (path_points[j][1] < path_points[i][1]):
-                c = not c
-        j = i
-    return c
-    
-    
-def construct_grid(path_points):
-    path = deque()
+# https://en.wikipedia.org/wiki/Pick%27s_theorem
+
+def get_boundary_points(path_points):
     current_x, current_y = 0, 0
+    path = [(current_x, current_y)]
     
-    while path_points:
-        direction, steps = path_points.popleft()
+    for direction, steps in path_points:
         steps = int(steps)
-        
+        dx, dy = 0, 0
         if direction == 'R':
-            for _ in range(steps):
-                current_y += 1
-                path.append((current_x, current_y))
+            dy = 1
         elif direction == 'L':
-            for _ in range(steps):
-                current_y -= 1
-                path.append((current_x, current_y))
+            dy = -1
         elif direction == 'U':
-            for _ in range(steps):
-                current_x -= 1
-                path.append((current_x, current_y))
+            dx = -1
         elif direction == 'D':
-            for _ in range(steps):
-                current_x += 1
-                path.append((current_x, current_y))
-                
-    # All the points cant be inserted into the matrix, so we need to find the max x and y
-    # Calculate the offset and insert the points into the matrix
-    
-    # Get min and max x and y from the path
-    min_x, min_y = min(path, key=lambda x: x[0])[0], min(path, key=lambda x: x[1])[1]
-    max_x, max_y = max(path, key=lambda x: x[0])[0], max(path, key=lambda x: x[1])[1]
-    
-    # Calculate the offset
-    if min_x < 0:
-        offset_x = abs(min_x)
-    else:
-        offset_x = 0
-        
-    if min_y < 0:
-        offset_y = abs(min_y)
-    else:
-        offset_y = 0
-        
-    # Initialize the grid to '.'s
-    grid = [['.' for _ in range(max_y + offset_y+1)] for _ in range(max_x + offset_x+1)]
-    # sum offsets to points in path
-    path = [(x+offset_x, y+offset_y) for x, y in path]
+            dx = 1
+        for _ in range(steps):
+            current_x += dx
+            current_y += dy
+            path.append((current_x, current_y))
+    return path
 
-    for row_idx in range(max_x + offset_x+1):
-        for col_idx in range(max_y + offset_y+1):
-            if (row_idx, col_idx) in path:
-                grid[row_idx][col_idx] = '#'
-            elif point_inside(row_idx, col_idx, path):
-                grid[row_idx][col_idx] = '#'
-   
-    return grid
-
+def calculate_area(path_points):
+    area = 0
+    n = len(path_points)
+    for i in range(n):
+        x1, y1 = path_points[i]
+        x2, y2 = path_points[(i + 1) % n]
+        area += (x1 * y2) - (x2 * y1)
+    return abs(area) // 2
 
 def main():
-    data = open("input.txt").read().strip().split("\n")
-    directions, steps = [], []
-    path_points = deque()
-    
-    for row_idx, line in enumerate(data):
-        directions.append(line[0]) 
-        steps.append(line[2]) 
-        path_points.insert(row_idx, (directions[-1], steps[-1]))
-        
-    grid = construct_grid(path_points)
-    
-    print('Part 1: ', sum([row.count('#') for row in grid]))
-       
+    data = [line.strip().split() for line in open("input.txt").readlines()]
+    path_points = deque([(d[0], d[1]) for d in data])
+
+    boundary_points = get_boundary_points(path_points)
+    area = calculate_area(boundary_points)
+    num_points = len(boundary_points)
+    number_inner_points = area - num_points // 2 + 1
+    print(number_inner_points + num_points)
+
 if __name__ == '__main__':
     main()
