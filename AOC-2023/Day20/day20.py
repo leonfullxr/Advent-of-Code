e7fe8c576458326471ff124b9eaec3d70b8a3be1
leon <l.elliottfuller@gmail.com>
2023-12-26 13:59:52 +0100

    Day20 part2 completed, need to clean code

--- AOC-2023/Day20/day20.py
+++ AOC-2023/Day20/day20.py
@@ -1,128 +1,109 @@
-from dataclasses import dataclass, field
 from enum import Enum
-import re
-from typing import Any, Self
+from collections import deque
 
-Signal = Enum("Signal", ["LOW", "HIGH"])
+class ModuleType(Enum):
+    FLIP_FLOP = "%"
+    CONJUCTION = "&"
+    BROADCASTER = "broadcaster"
 
-@dataclass
-class Node:
-    name: str = ""
-    children: list[Self] = field(default_factory=list)
 
-    def process(self, signal: Any) -> Any:
-        return []
+class PulseType(Enum):
+    LOW = 0
+    HIGH = 1
 
-    def add_inbound(self, node: Self):
-        return None
 
-@dataclass
-class SignalFire:
-    src: Node
-    dest: Node
-    value: Signal
+class Module:
+    def __init__(self, line: str):
+        name, destinations = line.split("->")
+        name = name.strip()
 
-@dataclass
-class BroadcasterNode(Node):
-    def process(self, signal: SignalFire) -> list[SignalFire]:
-        return [SignalFire(self, child, signal.value) for child in self.children]
-    
-
-@dataclass
-class FlipFlopNode(Node):
-    state: bool = False
-
-    def process(self, signal: SignalFire) -> list[SignalFire]:
-        if signal.value == Signal.LOW:
-            self.state = not self.state
-            if self.state == True:
-                return [SignalFire(self, child, Signal.HIGH) for child in self.children]
-            else:
-                return [SignalFire(self, child, Signal.LOW) for child in self.children]
-        return []
-
-@dataclass
-class ConjunctionNode(Node):
-    state: dict[str, Signal] = field(default_factory=dict)
-    
-    def process(self, signal: Any) -> list[SignalFire]:
-        self.state[signal.src.name] = signal.value
-        if all(value == Signal.HIGH for value in self.state.values()):
-            return [SignalFire(self, child, Signal.LOW) for child in self.children]
+        if name == "broadcaster":
+            self.type = ModuleType.BROADCASTER
+            self.name = name
         else:
-            return [SignalFire(self, child, Signal.HIGH) for child in self.children]
-        
-@dataclass
-class State():
-    nodes: dict[str, Node] = field(default_factory=dict)
-    start: BroadcasterNode = field(default_factory=BroadcasterNode)
+            self.type = ModuleType(name[0])
+            self.name = name[1:]
 
-    def process_press(self) -> dict[Signal, int]:
-        counts = {
-            Signal.LOW: 0,
-            Signal.HIGH: 0
-        }
-        queue = [SignalFire(Node("button"), self.start, Signal.LOW)]
-        while len(queue) != 0:
-            signal = queue.pop(0)
-            counts[signal.value] += 1
-            new_signals = signal.dest.process(signal)
-            queue.extend(new_signals)
-        return counts
+        self.destinations = [x.strip() for x in destinations.split(",")]
+        self.memory = False if self.type == ModuleType.FLIP_FLOP else {}
 
-    def calc_signal_sum(self, n: int):
-        counts = {
-            Signal.LOW: 0,
-            Signal.HIGH: 0
-        }
-        for _ in range(0, n):
-            round_counts = self.process_press()
-            counts[Signal.LOW] += round_counts[Signal.LOW]
-            counts[Signal.HIGH] += round_counts[Signal.HIGH]
-        print(counts)
-        return counts[Signal.LOW] * counts[Signal.HIGH]
+    def __repr__(self) -> str:
+        return (
+            self.name
+            + "{type="
+            + self.type.name
+            + " ("
+            + self.type.value
+            + "), destinations="
+            + str(self.destinations)
+            + ", memory="
+            + str(self.memory)
+            + "}"
+        )
 
-def parse_line_for_nodes(state: State, line: str, _idx: int) -> State:
-    if matches := re.match(r"^%([a-zA-Z]+)\s", line):
-        state.nodes[matches.group(1)] = FlipFlopNode(matches.group(1))
-    elif matches := re.match(r"^&([a-zA-Z]+)\s", line):
-        state.nodes[matches.group(1)] = ConjunctionNode(matches.group(1))
-    elif matches := re.match(r"^(broadcaster)\s", line):
-        state.start = state.nodes[matches.group(1)] = BroadcasterNode(matches.group(1))
-    else:
-        raise Exception(f"Could not parse line: {line}")
-    return state
+def process_input():
+    modules = {}
 
-def parse_line_for_edges(state: State, line: str, _idx: int) -> State:
-    if matches := re.match(r"^[&%]?([a-zA-Z]+) -> (.*)$", line):
-        children = matches.group(2).split(", ")
-        for child in children:
-            if child not in state.nodes:
-                print(f"Weird node: {child}")
-                state.nodes[child] = Node(child)
-            state.nodes[child].add_inbound(state.nodes[matches.group(1)])
-        state.nodes[matches.group(1)].children = [
-            state.nodes[child] for child in children
-        ]
-    else:
-        raise Exception(f"Could not parse line: {line}")
-    return state
+    for line in open("./input.txt").readlines():
+        module = Module(line)
 
+        modules[module.name] = module
 
-def process_file(state: State, filename: str, parse_functions: list):
-    with open(filename) as file:
-        for idx, line in enumerate(file):
-            line = line.strip()
-            for parse_func in parse_functions:
-                state = parse_func(state, line, idx)
-    return state
+    for name, module in modules.items():
+        for destination in module.destinations:
+            if (
+                destination in modules
+                and modules[destination].type == ModuleType.CONJUCTION
+            ):
+                modules[destination].memory[name] = PulseType.LOW
+    return modules
 
-def calculate(filename):
-    state = State()
-    # Primera fase: Parsear nodos
-    process_file(state, filename, [parse_line_for_nodes])
-    # Segunda fase: Parsear conexiones (edges)
-    process_file(state, filename, [parse_line_for_edges])
-    return state.calc_signal_sum(1000)
+def calculate_pulses(modules):
+    low_pulses = 0
+    high_pulses = 0
 
-print('Part 1', calculate("input.txt"))
+    for _ in range(1000):
+        low_pulses += 1
+
+        queue = deque(
+            [("broadcaster", x, PulseType.LOW) for x in modules["broadcaster"].destinations]
+        )
+
+        while queue:
+            source, destination, pulse = queue.popleft()
+
+            if pulse == PulseType.LOW:
+                low_pulses += 1
+            else:
+                high_pulses += 1
+
+            if destination not in modules:
+                continue
+
+            module = modules[destination]
+
+            if module.type == ModuleType.FLIP_FLOP:
+                if pulse == PulseType.LOW:
+                    module.memory = module.memory == False
+                    outgoing_pulse = PulseType.HIGH if module.memory else PulseType.LOW
+                    for dest in module.destinations:
+                        queue.append((module.name, dest, outgoing_pulse))
+
+            else:
+                module.memory[source] = pulse
+                outgoing_pulse = (
+                    PulseType.LOW
+                    if all(pulse == PulseType.HIGH for pulse in module.memory.values())
+                    else PulseType.HIGH
+                )
+                for dest in module.destinations:
+                    queue.append((module.name, dest, outgoing_pulse))
+    return low_pulses * high_pulses
+
+def main():
+    modules = process_input()
+    
+    print('Part 1', calculate_pulses(modules))
+    
+if __name__ == "__main__":
+    main()
