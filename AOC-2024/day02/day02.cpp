3c0ee5348e8d3a569b2b9b9641ca766c7bd7630b
leon <l.elliottfuller@gmail.com>
2024-12-08 13:34:42 +0100

    Changed the structure of the directories for better makefile processing

---
+++ AOC-2024/day02/day02.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "Day02 solution" << std::endl;
+    return 0;
+}
---
+++ AOC-2024/day03/day03.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "Day03 solution" << std::endl;
+    return 0;
+}
