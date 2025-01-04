7233f2bb975cc476adb84aa6512a1fd32b77ff85
leonfullxr <l.elliottfuller@gmail.com>
2025-01-04 13:26:26 +0100

    Completed day 8

--- AOC-2024/day08/day08.cpp
+++ AOC-2024/day08/day08.cpp
@@ -1,6 +1,4 @@
-#include <any>
 #include <iostream>
-#include <set>
 #include <vector>
 #include <fstream>
 #include <sstream>
@@ -52,7 +50,7 @@ bool is_in_bounds(int row, int col, const std::vector<std::vector<std::pair<char
     return row >= 0 && row < data.size() && col >= 0 && col < data[row].size();
 }
 
-int check_antinodes(std::vector<std::vector<std::pair<char,bool>>>& data, int row, int col) {
+int check_antinodes(std::vector<std::vector<std::pair<char,bool>>>& data, int row, int col, bool part2) {
     int count = 0;
     char word = data[row][col].first;
 
@@ -61,10 +59,21 @@ int check_antinodes(std::vector<std::vector<std::pair<char,bool>>>& data, int ro
             if (data[i][j].first == word and (i != row or j != col)) {
                 int row_diff = (row - i);
                 int col_diff = (col - j);
-                if (is_in_bounds(i-row_diff, j-col_diff, data)) {
+                if (is_in_bounds(i-row_diff, j-col_diff, data) and !part2) {
                     if (!data[i-row_diff][j-col_diff].second) {
                         count++;
                         data[i-row_diff][j-col_diff].second = true;
+                    }
+                } else if (part2) {
+                    int new_row = i+row_diff;
+                    int new_col = j+col_diff;
+                    while(is_in_bounds(new_row, new_col, data)) {
+                        if (!data[new_row][new_col].second) {
+                            count++;
+                            data[new_row][new_col].second = true;
+                        }
+                        new_row += row_diff;
+                        new_col += col_diff;
                     }
                 }
             }
@@ -74,13 +83,13 @@ int check_antinodes(std::vector<std::vector<std::pair<char,bool>>>& data, int ro
     return count;
 }
 
-int count_antinodes(std::vector<std::vector<std::pair<char,bool>>>& data) {
+int count_antinodes(std::vector<std::vector<std::pair<char,bool>>>& data, bool part2) {
    int count = 0;
 
    for (int row = 0; row < data.size(); row++) {
        for (int col = 0; col < data[row].size(); col++) {
            if (data[row][col].first != '.')
-            count += check_antinodes(data, row, col);
+            count += check_antinodes(data, row, col, part2);
        }
    }
 
@@ -90,11 +99,15 @@ int count_antinodes(std::vector<std::vector<std::pair<char,bool>>>& data) {
 int main() {
     std::vector<std::vector<std::pair<char,bool>>> data = parse_input("input.txt");
 
-    int total_antinodes = count_antinodes(data);
+    int total_antinodes = count_antinodes(data, false);
+
+    data = parse_input("input.txt");
+    int total_antinodes_part2 = count_antinodes(data, true);
 
     //print_matrix(data);
 
     std::cout << "Day08 solution " << total_antinodes << std::endl;
+    std::cout << "Day08 solution part2 " << total_antinodes_part2 << std::endl;
 
     return 0;
 }
